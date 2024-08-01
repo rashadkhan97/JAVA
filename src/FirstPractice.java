242804bb4f4f180590f1b3b00de1338ea6a074de
Md. Rashadul Islam <rsrashad99@gmail.com>
2024-08-01 12:33:27 +0600

    initial commit

--- src/FirstPractice.java
+++ src/FirstPractice.java
@@ -2,5 +2,6 @@ public class FirstPractice {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("*\n**");
+        System.out.println("Hello My name is Rashad");
     }
 }

a44cf8e9415ca415c1f0b43bc009326e9783bd01
Md. Rashadul Islam <rsrashad99@gmail.com>
2024-08-24 12:18:18 +0600

    Day 13

---
+++ src/arrays/intMinMaxExample.java
@@ -0,0 +1,35 @@
+//Find the maximum & minimum number in an array of integers.
+package arrays;
+
+import java.util.Scanner;
+
+public class intMinMaxExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the array index: ");
+        int arr = sc.nextInt();
+        int number[] = new int[arr];
+
+        //input
+        System.out.println("Enter the numbers: ");
+        for (int i=0; i<arr; i++){
+            number[i]=sc.nextInt();
+        }
+
+        int max = Integer.MIN_VALUE; //Integer.MIN_VALUE - a constant that holds the minimum value
+        int min = Integer.MAX_VALUE; //Integer.MAX_VALUE - a constant that holds the maximum value
+
+        //output
+        for (int i=0; i<number.length; i++) {
+            if (number[i] > max) {
+                max = number[i];
+            }
+            if (number[i] < min) {      //use if because of 2 condition min and max
+                min = number[i];
+            }
+        }
+        System.out.println("Max number: "+max);
+        System.out.println("Min number : "+min);
+    }
+
+}

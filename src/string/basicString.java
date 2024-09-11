a0c014297057f7970dd959308e36d5f2cedf81c2
Md. Rashadul Islam <rsrashad99@gmail.com>
2024-09-11 20:44:45 +0600

    day 17

---
+++ src/string/basicString.java
@@ -0,0 +1,17 @@
+//Take an array of names as input from the user and print them on the screen.
+package string;
+
+import java.util.Scanner;
+
+public class basicString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Name: ");
+
+        String name = sc.nextLine(); //If we can say if we print single word then use it - sc.next()
+                                    //But if we want to print spaces/ line  then it is - sc.nextLine()
+
+        System.out.print("Your Name is: "+name);
+
+    }
+}
